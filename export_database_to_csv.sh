30a87bd29b0dce411406e26f8ecf069741d15901
Bruno Fernández <84689264+bruferper@users.noreply.github.com>
2021-12-22 15:01:13 -0300

    Update export_database_to_csv.sh

--- export_database_to_csv.sh
+++ export_database_to_csv.sh
@@ -1,50 +1,55 @@
 #!/bin/bash
 
-# Script en bash para exportar los datos de las tablas de una base de datos en formato csv #
-# Cambiar los parametros de configuracion a los correspondientes de donde se desee correr #
+# Author: @bruferper #
 
-# Configuracion de parametros #
+# Bash script to export data of all tables to csv format #
+# Change the configuration parameters as needed #
+# NOTE: create in the /home directory the file .pgpass with 600 permissions (chmod 600 .pgpass) #
+# Add to the previous file the connection string with the following format: host:port:database:user:password #
+# Example: 127.0.0.1:5432:db1:user1:1234 #
+
+# Configuration parameters #
 HOST=127.0.0.1
 PORT=5432
-USER=usuario
-DB=prueba
+USER=user
+DB=testdb
 OUTPUT_DIR=/tmp/exports
 DELIMITER=";"
 
-echo "### Parametros ###"
-echo "Servidor: $HOST"
-echo "Puerto: $PORT"
-echo "Usuario: $USER"
-echo "Base de datos: $BD"
-echo "Directorio de salida: $OUTPUT_DIR"
-echo "Delimitador: $DELIMITER"
+echo "### Parameters ###"
+echo "Server: $HOST"
+echo "Port: $PORT"
+echo "User: $USER"
+echo "Database: $BD"
+echo "Output directory: $OUTPUT_DIR"
+echo "Delimiter: $DELIMITER"
 
-# Se limpia el directorio de salida #
+# Clean the output directory #
 rm -fr $OUTPUT_DIR/*
 
-# Se extrae la lista de tablas de la BD #
-TABLAS=$(psql -h $HOST -p $PORT -U $USER -d $DB -qtAc "SELECT table_name FROM information_schema.tables WHERE table_schema = 'public';")
+# Extract the list of tables #
+TABLES=$(psql -h $HOST -p $PORT -U $USER -d $DB -qtAc "SELECT table_name FROM information_schema.tables WHERE table_schema = 'public';")
 if [ $? -ne 0 ] ; then
-        echo "Error al conectarse al servidor de base de datos. Verificar datos de conexion."
+        echo "Error connecting to the server. Verify connection parameters."
         exit 1
 fi
 
-echo "### Comienzo del proceso ###"
+echo "### Start of process ###"
 
-# Se recorre cada tabla de la lista #
-for TABLA in $TABLAS
+# Iterate the list of tables #
+for TABLE in $TABLES
 do
 
-        # Se crea el archivo de salida #
-        OUTPUT_FILE=$TABLA.csv
+        # Creates the output file #
+        OUTPUT_FILE=$TABLE.csv
 
-        echo "$(date +"%d-%m-%Y %H:%M:%S") - Export de la tabla: $TABLA -> $OUTPUT_FILE"
+        echo "$(date +"%d-%m-%Y %H:%M:%S") - Export of table: $TABLE -> $OUTPUT_FILE"
 
-        psql -h $HOST -p $PORT -U $USER -d $DB -t -A -F $DELIMITER -c "SELECT * FROM $TABLA" > $OUTPUT_DIR/$OUTPUT_FILE
+        psql -h $HOST -p $PORT -U $USER -d $DB -t -A -F $DELIMITER -c "SELECT * FROM $TABLE" > $OUTPUT_DIR/$OUTPUT_FILE
         if [ $? -ne 0 ] ; then
-                echo "Error al realizar el export de la tabla."
+                echo "Error performing the export."
         fi
 
 done
 
-echo "### Fin del proceso ###"
+echo "### End of process ###"
